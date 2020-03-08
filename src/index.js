914d097d70a84c855b50f28b7e33add6e98d7eba
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-03-08 15:17:57 +0300

    solve reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = n.toString().split('')
+    if (n[0] == '-') n = n.slice(1)
+    return Number(n.reverse().join(''))
 }
